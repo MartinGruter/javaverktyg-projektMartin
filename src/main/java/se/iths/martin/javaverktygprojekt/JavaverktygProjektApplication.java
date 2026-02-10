d254406f351c246434c760b8bca2aaaaf15a6b68
MartinGruter <Martin.Grter@iths.se>
2026-02-10 10:30:43 +0100

    Grunden för grupparbete

---
+++ src/main/java/se/iths/martin/javaverktygprojekt/JavaverktygProjektApplication.java
@@ -0,0 +1,13 @@
+package se.iths.martin.javaverktygprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaverktygProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaverktygProjektApplication.class, args);
+    }
+
+}
