7ebae6bffc632814b9d9e9f06b4458c23fd97aa4
Mariapalm98 <mariapalma.mpp@gmail.com>
2026-02-11 12:40:46 +0100

    Order klasser Model sql och repository

---
+++ src/main/java/se/iths/martin/javaverktygprojekt/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package se.iths.martin.javaverktygprojekt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.iths.martin.javaverktygprojekt.model.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+}
