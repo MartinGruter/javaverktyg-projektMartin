87a85813bd8667378aadab78684fbb2c4933d421
kevindbois <kevin.dbois@iths.se>
2026-02-20 14:23:58 +0100

    fixed sql-naming

--- sql/Review.sql
+++ sql/Review.sql
@@ -1,9 +1,9 @@
 CREATE TABLE review (
     id BIGSERIAL PRIMARY KEY,
-    reviewerName VARCHAR(50) NOT NULL,
+    reviewer_name VARCHAR(50) NOT NULL,
     rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
     title VARCHAR(80) NOT NULL,
     comment VARCHAR(500) NOT NULL,
-    createdDate DATE NOT NULL
+    created_date DATE NOT NULL
 
 );
